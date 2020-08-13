76da1a9073553653952e93771913e11828ee131d
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-08-13 10:51:54 -0400

    added modal for sign in

--- src/features/nav/SignedOutMenu.jsx
+++ src/features/nav/SignedOutMenu.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { Button, Menu } from "semantic-ui-react";
+import { openModal } from "../../app/common/modals/modalReducer";
 
 export default function SignedOutMenu({ setAuthenticated }) {
+  const dispatch = useDispatch();
   return (
     <Menu.Item position='right'>
       <Button
         basic
         inverted
         content='Login'
-        onClick={() => setAuthenticated(true)}
+        onClick={() => dispatch(openModal({ modalType: "LoginForm" }))}
       />
       <Button
         basic

94fee4b978dfcfd02d42bec3004abdf1e2567cc6
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-08-16 10:16:28 -0400

    add async code for loading + set up ui for event filters

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -8,6 +8,9 @@ import { decrement, increment } from "./testReducer";
 
 export default function Sandbox() {
   const dispatch = useDispatch();
+  const [target, setTarget] = useState(null);
+  const data = useSelector((state) => state.test.data);
+  const { loading } = useSelector((state) => state.async);
   const defaultProps = {
     center: {
       lat: 59.95,
@@ -15,7 +18,6 @@ export default function Sandbox() {
     },
     zoom: 11,
   };
-  const data = useSelector((state) => state.test.data);
   const [location, setLocation] = useState(defaultProps);
 
   function handleSetLocation(latLng) {
@@ -27,14 +29,24 @@ export default function Sandbox() {
       <h1>Testing 123</h1>
       <h3>The data is: {data} </h3>
       <Button
-        onClick={() => dispatch(increment(20))}
+        name='increment'
+        onClick={(event) => {
+          dispatch(increment(20));
+          setTarget(event.target.name);
+        }}
         content='Increment'
         color='green'
+        loading={loading && target === "increment"}
       />
       <Button
-        onClick={() => dispatch(decrement(10))}
+        name='decrement'
+        onClick={(event) => {
+          dispatch(decrement(10));
+          setTarget(event.target.name);
+        }}
         content='Decrement'
         color='red'
+        loading={loading && target === "decrement"}
       />
       <Button
         onClick={() =>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
+import "react-calendar/dist/Calendar.css";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.min.css";
 import "semantic-ui-css/semantic.min.css";
 import App from "./app/layout/App.jsx";
 import ScrollToTop from "./app/layout/ScrollToTop.jsx";
 import "./app/layout/styles.css";
 import { configureStore } from "./app/store/configureStore";
+import { loadEvents } from "./features/events/eventActions.js";
 import * as serviceWorker from "./serviceWorker";
 
 const store = configureStore();
+
+store.dispatch(loadEvents());
+
 const rootElement = document.getElementById("root");
 
 function render() {
